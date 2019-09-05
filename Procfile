77b05816924ec9d41e3f8b2fb73d400b15815ed1
aldclopes <aldclopes.geral@gmail.com>
2019-09-05 13:00:08 +0100

    Populate2 in Procfile

--- Procfile
+++ Procfile
@@ -1,4 +1,3 @@
 web: gunicorn ocp.wsgi --log-file -
-python manage.py populate_ocp.py
+worker: python manage.py populate_ocp.py
 worker: celery -A ocp worker --loglevel=info
-
--- ocp/settings/production.py
+++ ocp/settings/production.py
@@ -9,20 +9,21 @@ DATABASES = {
     'default':  dj_database_url.config(),
 }
 
-
 # # ########################## AWS S3 Private Media Upload
 if USE_S3:
     # # ########################## AWS S3 - Settings Variable
     AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # # ########################## AWS S3 Static files (CSS, JavaScript, Images)
+   # # ########################## AWS S3 Static files (CSS, JavaScript, Images)
+    # # Amazon S3/myBucketName/static/myimage/files
     STATICFILES_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    # STATIC_ROOT = [os.path.join(BASE_DIR, 'mypage/static'), ]  # Não usar com S3
     STATICFILES_STORAGE = config('AWS_STATICFILES_STORAGE')
 
     # # ########################## AWS S3 Private Media Upload
-    # # Amazon S3/testheroku-assets/media/myimage/files
+    # # Amazon S3/myBucketName/media/myimage/files
     MEDIAFILES_LOCATION = 'media'
 
     # https://testheroku-assets.s3.amazonaws.com/media/myimage/images/Course_6.png
@@ -30,7 +31,8 @@ if USE_S3:
     MEDIA_URL = f'https://{AWS_S3_PATH}/{MEDIAFILES_LOCATION}/'
 
     # MEDIA_ROOT = os.path.join(BASE_DIR, 'mypage/media') # Não usar com S3
-    DEFAULT_FILE_STORAGE = 'testheroku.storage_backends.MediaStorage'
+    DEFAULT_FILE_STORAGE = config('AWS_DEFAULT_FILE_STORAGE')
+
 
 
 
